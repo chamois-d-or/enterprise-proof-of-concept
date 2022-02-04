56c5e74f942cad7b76afcbbfbfa9c3965531fedb
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2022-02-04 16:09:27 +0100

    add freecaster slice

--- slices/ecommerce/FreeCasterSlice/index.js
+++ slices/ecommerce/FreeCasterSlice/index.js
@@ -4,11 +4,11 @@ import { RichText } from 'prismic-reactjs'
 const FreeCasterSlice = ({ slice }) => (
   <section>
    <div className="section-header">
-      <div className="header-video m-hide  voile-noir">
+      <div className="header-video m-hide bg-black voile-noir">
         {
           slice.primary.video?
-          <video className="fp-engine" playsInline webkit-playsinline loop autoplay="true" muted="true" src={slice.primary.video.video_url} />
-          :<video className="fp-engine" playsInline webkit-playsinline loop autoplay="true" muted="true" src="https://vod.fl.freecaster.net/vod/lvmh/wG1Ekry2k2_1080p.mp4" />
+          <video className="h-screen w-screen fp-engine" playsInline webkit-playsinline loop autoplay="true" muted="true" src={slice.primary.video.video_url} />
+          :<video className="h-screen w-screen fp-engine" playsInline webkit-playsinline loop autoplay="true" muted="true" src="https://vod.fl.freecaster.net/vod/lvmh/wG1Ekry2k2_1080p.mp4" />
         }
       </div>
   </div>
